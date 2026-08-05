b6cde35d16f3ddb968dc321da44e536a1d8f51eb
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2026-08-06 00:13:13 +0300

    feat(auth-service): implement response DTOs and mappers for login and registration services

--- apps/auth-service/src/auth-service.controller.ts
+++ apps/auth-service/src/auth-service.controller.ts
@@ -1,6 +1,8 @@
 import {Body, Controller, Post} from '@nestjs/common';
 import {AuthServiceService} from './auth-service.service';
 import {LoginRequestDto, RegisterRequestDto} from "@app/contracts";
+import {RegisterResponseDto} from "@app/contracts/auth/dto/register-response.dto";
+import {LoginResponseDto} from "@app/contracts/auth/dto/login-response.dto";
 
 @Controller('auth')
 export class AuthServiceController {
@@ -8,12 +10,12 @@ export class AuthServiceController {
     }
 
     @Post('register')
-    register(@Body() registerRequest: RegisterRequestDto) {
+    register(@Body() registerRequest: RegisterRequestDto): Promise<RegisterResponseDto> {
         return this.authServiceService.register(registerRequest);
     }
 
     @Post('login')
-    login(@Body() loginRequest: LoginRequestDto) {
+    login(@Body() loginRequest: LoginRequestDto): Promise<LoginResponseDto> {
         return this.authServiceService.login(loginRequest);
     }
 }
